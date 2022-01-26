dea43db64f42f8c521009d38da119ea227a9ed8e
user <spawn.2010@mail.ru>
2022-01-26 19:31:42 +0300

    Wrap actions in callbacks

--- public/index.php
+++ public/index.php
@@ -1,19 +1,75 @@
 <?php
 
-use Framework\Http\RequestFactory;
-use Framework\Http\Response;
+use Psr\Http\Message\ServerRequestInterface;
+use Zend\Diactoros\ServerRequestFactory;
+use Zend\Diactoros\Response\HtmlResponse;
+use Laminas\HttpHandlerRunner\Emitter\SapiEmitter;
+use Zend\Diactoros\Response\JsonResponse;
 
 chdir(dirname(__DIR__));
 require 'vendor/autoload.php';
 
-$request = RequestFactory::fromGlobals();
+#init
 
-$name = $request->getQueryParams()['name'] ?? 'Guest';
+$request = ServerRequestFactory::fromGlobals();
 
-$response =  (new Response ('Hello, '. $name . '!'))->withHeader('X-Developer', 'ElisDN');
+#Action
 
-header('HTTP/1.0 '. $response->getStatusCode() . ' ' . $response->getReasonPhrase());
-foreach ($response->getHeaders() as $name => $value){
-    header ($name . ':' . $value);
+### Action
+
+$path = $request->getUri()->getPath();
+$action = null;
+
+if ($path === '/') {
+
+    $action = function (ServerRequestInterface $request) {
+        $name = $request->getQueryParams()['name'] ?? 'Guest';
+        return new HtmlResponse('Hello, ' . $name . '!');
+    };
+
+} elseif ($path === '/about') {
+
+    $action = function () {
+        return new HtmlResponse('I am a simple site');
+    };
+
+} elseif ($path === '/blog') {
+
+    $action = function () {
+        return new JsonResponse([
+            ['id' => 2, 'title' => 'The Second Post'],
+            ['id' => 1, 'title' => 'The First Post'],
+        ]);
+    };
+
+} elseif (preg_match('#^/blog/(?P<id>\d+)$#i', $path, $matches)) {
+
+    $request = $request->withAttribute('id', $matches['id']);
+
+    $action = function (ServerRequestInterface $request) {
+        $id = $request->getAttribute('id');
+        if ($id > 2) {
+            return new JsonResponse(['error' => 'Undefined page'], 404);
+        }
+        return new JsonResponse(['id' => $id, 'title' => 'Post #' . $id]);
+    };
+
 }
-echo $response->getBody();
+
+if ($action) {
+    $response = $action($request);
+} else {
+    $response = new HtmlResponse('Undefined page', 404);
+}
+
+
+#Postprocessing
+
+$response->withHeader('X-Developer', 'ElisDN');
+
+#Sending
+
+$emitter = new SapiEmitter();
+$emitter->emit($response);
+
+
