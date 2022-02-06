01e97ed0521023eabfa4f4af3f87244b95c4ab71
spawn2010 <spawn.2010@mail.ru>
2022-02-06 20:52:17 +0300

    Moved parameters into container

---
+++ src/Framework/Container/Container.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Framework\Container;
+
+class Container
+{
+    private $definitions = [];
+    private $results = [];
+
+    public function get($id)
+    {
+        if (array_key_exists($id, $this->results)) {
+            return $this->results[$id];
+        }
+
+        if (!array_key_exists($id, $this->definitions)) {
+            throw new ServiceNotFoundException('Unknown service "' . $id . '"');
+        }
+
+        $definition = $this->definitions[$id];
+
+        if ($definition instanceof \Closure) {
+            $this->results[$id] = $definition($this);
+        } else {
+            $this->results[$id] = $definition;
+        }
+
+        return $this->results[$id];
+    }
+
+    public function set($id, $value): void
+    {
+        if (array_key_exists($id, $this->results)) {
+            unset($this->results[$id]);
+        }
+        $this->definitions[$id] = $value;
+    }
+}
---
+++ tests/Framework/Container/ContainerTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Tests\Framework\Container;
+
+use Framework\Container\Container;
+use Framework\Container\ServiceNotFoundException;
+use PHPUnit\Framework\TestCase;
+
+class ContainerTest extends TestCase
+{
+    public function testPrimitives()
+    {
+        $container = new Container();
+
+        $container->set($name = 'name', $value = 5);
+        self::assertEquals($value, $container->get($name));
+
+        $container->set($name = 'name', $value = 'string');
+        self::assertEquals($value, $container->get($name));
+
+        $container->set($name = 'name', $value = ['array']);
+        self::assertEquals($value, $container->get($name));
+
+        $container->set($name = 'name', $value = new \stdClass());
+        self::assertEquals($value, $container->get($name));
+    }
+
+    public function testCallback()
+    {
+        $container = new Container();
+
+        $container->set($name = 'name', function () {
+            return new \stdClass();
+        });
+
+        self::assertNotNull($value = $container->get($name));
+        self::assertInstanceOf(\stdClass::class, $value);
+    }
+
+    public function testContainerPass()
+    {
+        $container = new Container();
+
+        $container->set('param', $value = 15);
+        $container->set($name = 'name', function (Container $container) {
+            $object = new \stdClass();
+            $object->param = $container->get('param');
+            return $object;
+        });
+
+        self::assertObjectHasAttribute('param', $object = $container->get($name));
+        self::assertEquals($value, $object->param);
+    }
+
+    public function testSingleton()
+    {
+        $container = new Container();
+
+        $container->set($name = 'name', function () {
+            return new \stdClass();
+        });
+
+        self::assertNotNull($value1 = $container->get($name));
+        self::assertNotNull($value2 = $container->get($name));
+        self::assertSame($value1, $value2);
+    }
+
+    public function testNotFound()
+    {
+        $container = new Container();
+
+        $this->expectException(ServiceNotFoundException::class);
+
+        $container->get('email');
+    }
+}
+
