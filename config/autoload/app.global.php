6bed2a9432b6858cd57c6da61fae40fc855df93e
spawn2010 <spawn.2010@mail.ru>
2022-02-07 22:44:41 +0300

    Installed config aggregator

--- config/autoload/app.global.php
+++ config/autoload/app.global.php
@@ -1,10 +1,7 @@
 <?php
 
-use App\Http\Action;
 use App\Http\Middleware;
 use Framework\Http\Application;
-use Framework\Http\Middleware\DispatchMiddleware;
-use Framework\Http\Middleware\RouteMiddleware;
 use Framework\Http\Pipeline\MiddlewareResolver;
 use Framework\Http\Router\AuraRouterAdapter;
 use Framework\Http\Router\Router;
@@ -12,20 +9,14 @@ use Psr\Container\ContainerInterface;
 
 return [
     'dependencies' => [
-        'invokables' => [
-            Middleware\CredentialsMiddleware::class,
-            Middleware\ProfilerMiddleware::class,
-            Action\HelloAction::class,
-            Action\AboutAction::class,
-            Action\CabinetAction::class,
-            Action\Blog\IndexAction::class,
-            Action\Blog\ShowAction::class,
+        'abstract_factories' => [
+            Zend\ServiceManager\AbstractFactory\ReflectionBasedAbstractFactory::class,
         ],
         'factories' => [
             Application::class => function (ContainerInterface $container) {
                 return new Application(
                     $container->get(MiddlewareResolver::class),
-                    $container->get(Framework\Http\Router\Router::class),
+                    $container->get(Router::class),
                     new Middleware\NotFoundHandler(),
                     new Zend\Diactoros\Response()
                 );
@@ -36,18 +27,11 @@ return [
             MiddlewareResolver::class => function (ContainerInterface $container) {
                 return new MiddlewareResolver($container);
             },
-            Middleware\BasicAuthMiddleware::class => function (ContainerInterface $container) {
-                return new Middleware\BasicAuthMiddleware($container->get('config')['users']);
-            },
             Middleware\ErrorHandlerMiddleware::class => function (ContainerInterface $container) {
                 return new Middleware\ErrorHandlerMiddleware($container->get('config')['debug']);
             },
-            DispatchMiddleware::class => function (ContainerInterface $container) {
-                return new DispatchMiddleware($container->get(MiddlewareResolver::class));
-            },
-            RouteMiddleware::class => function (ContainerInterface $container) {
-                return new RouteMiddleware($container->get(Router::class));
-            },
         ],
     ],
+
+    'debug' => false,
 ];
--- config/config.php
+++ config/config.php
@@ -1,6 +1,10 @@
 <?php
 
-return array_merge_recursive(
-    require __DIR__ . '/dependencies.php',
-    require __DIR__ . '/parameters.php'
-);
+use Zend\ConfigAggregator\ConfigAggregator;
+use Zend\ConfigAggregator\PhpFileProvider;
+
+$aggregator = new ConfigAggregator([
+    new PhpFileProvider(__DIR__ . '/autoload/{{,*.}global,{,*.}local}.php'),
+]);
+
+return $aggregator->getMergedConfig();
