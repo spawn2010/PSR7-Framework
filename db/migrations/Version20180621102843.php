edb68ae507e9ded28892c9dccbab6b0579f77e2b
user <spawn.2010@mail.ru>
2022-02-11 12:58:45 +0300

    Changed posts display

--- db/migrations/Version20180621102843.php
+++ db/migrations/Version20180621102843.php
@@ -8,19 +8,19 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220210154713 extends AbstractMigration
+final class Version20180621102843 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
 
-        $this->addSql('CREATE TABLE comments (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, post_id INTEGER NOT NULL, date DATETIME NOT NULL --(DC2Type:datetime_immutable)
-        , author VARCHAR(255) NOT NULL, text CLOB NOT NULL)');
-        $this->addSql('CREATE INDEX IDX_5F9E962A4B89032C ON comments (post_id)');
         $this->addSql('CREATE TABLE posts (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, create_date DATETIME NOT NULL --(DC2Type:datetime_immutable)
         , update_date DATETIME DEFAULT NULL --(DC2Type:datetime_immutable)
         , title VARCHAR(255) NOT NULL, content_short CLOB NOT NULL, content_full CLOB NOT NULL, meta_title VARCHAR(255) DEFAULT NULL, meta_description VARCHAR(255) DEFAULT NULL)');
+        $this->addSql('CREATE TABLE comments (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, post_id INTEGER NOT NULL, date DATETIME NOT NULL --(DC2Type:datetime_immutable)
+        , author VARCHAR(255) NOT NULL, text CLOB NOT NULL)');
+        $this->addSql('CREATE INDEX IDX_5F9E962A4B89032C ON comments (post_id)');
     }
 
     public function down(Schema $schema) : void
@@ -28,7 +28,7 @@ final class Version20220210154713 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
 
-        $this->addSql('DROP TABLE comments');
         $this->addSql('DROP TABLE posts');
+        $this->addSql('DROP TABLE comments');
     }
 }
